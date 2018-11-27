4a53044b2909be838ef039a3b5d3755d95daad8f
Charlotte Bruzeau <charlotte.bruzeau@yahoo.fr>
2018-11-27 11:42:08 +0100

    modifs modèles

---
+++ Models/logements.php
@@ -0,0 +1,15 @@
+<?php
+
+require("database.php");
+
+// Ajouter un logement 
+
+function ajoutLogement($db){
+    
+}
+
+// récupérer les infos du logement
+// Modifier les infos du logement 
+// supprimer un logement 
+
+?>
