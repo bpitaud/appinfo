20c844b9e9611c87b07d45832cbfd7d56644fc2d
alexisbdb <alexis.deberc@gmail.com>
2018-11-29 15:50:15 +0100

    confclient
    wxcvb

--- ConfirmationClient/ConfClient.php
+++ ConfirmationClient/ConfClient.php
@@ -11,50 +11,44 @@
 
 		<!--HEADER-->
 
-    <header>
-      <div class="wrapper">
-        <h1><strong>DOMISEP</strong><br/>Administrateur</h1> 	
-           <ul>
-             <li> 					
-              <div class="dropdownLang">
-                <div class="noHover">
-                  <p>FR</p>
-                </div>
-                <div class="hover">
-                  <p>FR</p>
-                  <a href="english.html"> EN </a>
-                </div>
-              </div>
-            </li>
-          </ul>
-           <p class="admin"> SAV Client : ADRESSE.EMAIL@mail.com</p>
-             
-          <a class="quitter" href="../RechercherPar/RechercherPar.php"><span>Quitter</span></a>
-      </div>
-    </header>
+<header>
+  <div class="wrapper">
+    <h1><strong>DOMISEP</strong><br/>Administrateur</h1> 	
+       <ul>
+         <li> 					
+          <div class="dropdownLang">
+            <div class="noHover">
+              <p>FR</p>
+            </div>
+            <div class="hover">
+              <p>FR</p>
+              <a href="english.html"> EN </a>
+            </div>
+          </div>
+        </li>
+      </ul>
+       <p class="admin"> SAV Client : ADRESSE.EMAIL@mail.com</p>
+         
+      <a class="quitter" href="../RechercherPar/RechercherPar.php"><span>Quitter</span></a>
+  </div>
+</header>
 
     		<!--CODE DE LA PAGE-->
 
-    			<h2>Confirmation Client</h2>
+    <h2>Confirmation Client</h2>
 
     <div id="conteneur">
     	<div class="texte">
-				<p>Nom : NOM</p> 
-				</br>
-				<p>Prénom : PRENOM</p> 
-				</br>
-				<p>Numéro de téléphone : +33 0 00 00 00 00</p> 
-				</br>
-				<p>Adresse email : ADRESSE.EMAIL@MAIL.COM</p> 
-    	</div>
-    
-
-    
-		<div class="bouton">
-		<input type=button onclick=window.location.href="../Menu/Menu.php"; value=Valider />
-
-		</div>
-	</div>
+				<em classe="espace">Nom : NOM</br></em>
+				<em classe="espace">Prénom : PRENOM</br></em>
+				<em classe="espace">Numéro de téléphone : +33 0 00 00 00 00</br></em>
+				<em classe="espace">Adresse email : ADRESSE.EMAIL@MAIL.COM </em>
+          
+    		<div class="bouton">
+    		<input type=button onclick=window.location.href="../Menu/Menu.php"; value=Valider />
+    		</div>
+  	   </div>
+    </div>
 
     <footer>
     	<p> Connecté en tant que : ADRESSE_EMAIL_ADMIN</p>
