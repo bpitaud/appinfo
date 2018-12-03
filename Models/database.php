c843d69fe7e34dbee4c1afb5c97a9ae6f24fd629
CarroWilliam <38757038+CarroWilliam@users.noreply.github.com>
2018-12-03 11:15:23 +0100

    modif

--- Models/database.php
+++ Models/database.php
@@ -1,16 +1,18 @@
-<?php
+<?php 
 $servername = "localhost";
 $user = "root";
 $password = "root";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=Domisep", $user, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
-?>
+function connect (){
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=Domisep", $user, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully"; 
+        return $conn;
+        }
+    catch(PDOException $e)
+        {
+        return echo "Connection failed: " . $e->getMessage(); 
+        }
+}
+ ?>
