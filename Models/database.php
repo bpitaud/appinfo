12ab799b905c807dfaf5fd9539b54771161e7336
Charlotte Bruzeau <charlotte.bruzeau@yahoo.fr>
2018-12-04 11:10:53 +0100

    modif database.php

--- Models/database.php
+++ Models/database.php
@@ -2,9 +2,9 @@
 function connect (){
 $servername = "localhost";
 $user = "root";
-$password = "";
+$password = "root";
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=domisep",$user,$password);
+        $conn = new PDO("mysql:host=$servername;dbname=Domisep",$user,$password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully"; 
