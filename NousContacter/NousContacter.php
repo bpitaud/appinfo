607f3ca120c031f9299f4ec2990c470029d95d50
Bruno <brunopitaud@gmail.com>
2018-12-11 11:59:29 +0100

    bruno

--- NousContacter/NousContacter.php
+++ NousContacter/NousContacter.php
@@ -44,10 +44,19 @@
     </header>
     <section>
         <h2>Nous contacter</h2>
+        <div class="page">
+		<div id="coneteneur1">
+        <p>35 rue Notre-Dame des champs,</br> Paris, 75006,France</p>
+        </div>
+        <div id="coneteneur2">
+        <p>+33 (0) 1 55 09 24 92</p>
+        </div>
+        <div id="coneteneur3">
+        <p>contact@domisep.fr</p>
+        </div>
 
-        <em>35 rue Notre-Dame des champs, Paris, 75006,France</em>
 
-
+        </div>   
 
 
     </section>
