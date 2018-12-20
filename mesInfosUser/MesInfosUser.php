f8d81ad3f18036e5c49c32f43625d955da24e85e
Bruno <brunopitaud@gmail.com>
2018-12-20 17:24:56 +0100

    Modis mes infos users

--- mesInfosUser/MesInfosUser.php
+++ mesInfosUser/MesInfosUser.php
@@ -76,13 +76,14 @@ function myFunction() {
 }
 </script>
 
+
+
 <div class="button1">
 <a href="../ModifCompteUser/ModifCompteUser.php"> <input type="button" value="Modifier mes informations"></a>               
 </div>   
 
-<div class="button2"
+<div class="button2">
 <a href="../reinitialisationmdp/réuni.php"> <input type="button" value ="Réinitialiser mon mot de passe"></a> </div>
-            </p>
     </div>
     	
     		
