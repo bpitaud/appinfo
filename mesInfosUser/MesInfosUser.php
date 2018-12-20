859c74978a1c78227ffa515ddd72e0fc7c1c917a
Grimars <grimaldi_emma@yahoo.fr>
2018-12-20 17:28:42 +0100

    suppression réinitialiser mot de passe

--- mesInfosUser/MesInfosUser.php
+++ mesInfosUser/MesInfosUser.php
@@ -80,10 +80,7 @@ function myFunction() {
 <a href="../ModifCompteUser/ModifCompteUser.php"> <input type="button" value="Modifier mes informations"></a>               
 </div>   
 
-<div class="button2"
-<a href="../reinitialisationmdp/réuni.php"> <input type="button" value ="Réinitialiser mon mot de passe"></a> </div>
-            </p>
-    </div>
+
     	
     		
     			
