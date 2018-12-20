f237e7d6eb74e507cd694cf1b0c9d63321f87270
ChaBru97 <charlotte.bruzeau@yahoo.fr>
2018-12-20 15:01:55 +0100

    modifs ajout capteur

--- controllers/FormulaireAjoutCapteur.php
+++ controllers/FormulaireAjoutCapteur.php
@@ -4,15 +4,56 @@ require_once("../Models/capteurs.php");
 
 session_start();
 // define variables and set to empty values
+
+
+$capteurs = array("lumiere", "camera", "humidite", "temperature");
+$controleurs = array("chauffage");
+
 $name = $capteurID = $type = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = test_input($_POST["name"]);
     $type = test_input($_POST["type"]);
-    $capteurID = test_input($_POST["number"]);
-    $pieceID = $_SESSION['pieceID'];
-    ajoutCapteur($nom, $capteurID, $type, $pieceID, 1);
-    header('Location: ../Liste capteurs/listecapteurs.php');
+    if (in_array($type, $capteurs)){
+      switch (type){
+        case "lumiere":
+        $capteurID = test_input($_POST["number"]);
+        $pieceID = $_SESSION['pieceID'];
+        ajoutCapteur($nom, $capteurID, $type, $pieceID, 1);
+        header('Location: ../Liste capteurs/listecapteurs.php');
+        break;
+
+        case "camera":
+        $capteurID = test_input($_POST["number"]);
+        $pieceID = $_SESSION['pieceID'];
+        ajoutCapteur($nom, $capteurID, $type, $pieceID, 1);
+        header('Location: ../Liste capteurs/listecapteurs.php');
+        break;
+
+        case "humidite":
+        $capteurID = test_input($_POST["number"]);
+        $pieceID = $_SESSION['pieceID'];
+        ajoutCapteur($nom, $capteurID, $type, $pieceID, 1);
+        header('Location: ../Liste capteurs/listecapteurs.php');
+        break;
+
+        case "temperature":
+        $capteurID = test_input($_POST["number"]);
+        $pieceID = $_SESSION['pieceID'];
+        ajoutCapteur($nom, $capteurID, $type, $pieceID, 1);
+        header('Location: ../Liste capteurs/listecapteurs.php');
+        break;
+      }
+    } else {
+      switch ($type){
+        case "camera":
+        $controleurID = test_input($_POST["number"]);
+        $pieceID = $_SESSION['pieceID'];
+        ajoutControleur($nom, $controleurID, $type, $pieceID, 1);
+        header('Location: ../Liste capteurs/listecapteurs.php');
+        break;
+      }
+    }
 }
 
 function test_input($data) {
