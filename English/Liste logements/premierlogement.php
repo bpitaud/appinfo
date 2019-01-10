d56855f1b517d21891d4d2cd741d2397ad1238a8
Bruno <brunopitaud@gmail.com>
2019-01-10 17:34:36 +0100

    Ajout pages anglais

---
+++ English/Liste logements/premierlogement.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="premierlogement.css" />
+        <title>Domisep - Liste des pièces</title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    </head>
+
+    <body>
+        <header>
+        <div class="wrapper">
+            <h1>DOMISEP</h1>
+            <nav>
+                <ul>
+                    <li><a href="../Liste logements/premierlogement.php"><span>Home</span></a></li>
+                    <li>
+                        <div class="dropdownLang">
+                            <div class="noHover">
+                                <p>FR</p>
+                            </div>
+                            <div class="hover">
+                                <p>FR</p>
+                                <a href="english.html"> EN </a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <div id="noHoverUser">
+                                    <button class="boutonmenuprincipal"><p></p></button>
+                            </div>
+                            <div id="hoverUser">
+                                <button class="boutonmenuprincipal"><p></p></button>
+                                <a href="../mesInfosUser/MesInfosUser.php"><p>Mes infos</p></a>
+                                <a href="NousContacter.html"><p>Contacter</p></a>
+                                <a href="../controllers/deconnexion.php"><p id="borderNone">Deconnexion</p></a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <section>
+    	<div id="logement">    
+                <p> <span> Bienvenue sur Domisep ! </span> <br/> Vous pouvez dès à présent créer votre premier logement </p>
+                <div class="ajoutlogement">
+                    <a href="../AjoutLogement/AjoutLogement.php"> +  Ajouter un logement </a>
+                </div>
+        </div>	
+
+        
+    </section>
+
+    <footer>
+    	<p> WEBAC © Tous droits réservés </p>
+    </footer>
+    
+</body>
---
+++ English/mesInfosUser/MesInfosUser.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="MesInfosUser.css" />
+        <title>Domisep - Mes informations</title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    </head>
+
+    <body>
+        <header>
+        <div class="wrapper">
+            <h1>DOMISEP</h1>
+            <nav>
+                <ul>
+                    <li><a href="../Liste logements/listelogements.php"><span>Home</span></a></li>
+                    <li>
+                        <div class="dropdownLang">
+                            <div class="noHover">
+                                <p>FR</p>
+                            </div>
+                            <div class="hover">
+                                <p>FR</p>
+                                <a href="english.html"> EN </a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <div id="noHoverUser">
+                                    <button class="boutonmenuprincipal"><p></p></button>
+                            </div>
+                            <div id="hoverUser">
+                                <button class="boutonmenuprincipal"><p></p></button>
+                                <a href="../mesInfosUser/MesInfosUser.php"><p>Mes infos</p></a>
+                                <a href="../NousContacter/NousContacter.php"><p>Contacter</p></a>
+                                <a href="Accueil.html"><p id="borderNone">Deconnexion</p></a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    
+    <h2> Mes informations </h2>
+<section>
+    <div class="info">
+    		<p>
+    			<em class="base">ADRESS.EMAIL@MAIL.COM <br/></em>
+    			<em class="base">NOM<br/></em>
+    			<em class="base">PRENOM<br/></em>
+    			<em class="base">GENRE<br/></em>
+    			<em class="base2">JJ/MM/AAAA<br/></em>
+                <em class="base">+33 6 00 00 00 00<br/></em>
+    			<em class="base">ADRESSE PRINCIPALE<br/></em>
+    			<em class="base">PAYS<br/></em>
+    			<em class="base2">75 000<br/></em>
+                <em class="base">+33 6 00 00 00 00<br/></em>
+    			<em class="base">ADRESSE PRINCIPALE<br/></em>
+    			<em class="base">PAYS<br/></em>
+    			<em class="base2">75 000<br/></em>
+                <button onclick="myFunction()" class="bouton">Supprimer mon compte</button>
+
+
+<script>
+function myFunction() {
+    var txt;
+    var r = confirm("Etes-vous sûr de vouloir supprimer votre compte ?");
+    if (r == true) {
+        href = "../RechercherPar/RechercherPar.php";
+    } else {
+        txt = "none";
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
+</script>
+
+
+
+<div class="button1">
+<a href="../ModifCompteUser/ModifCompteUser.php"> <input type="button" value="Modifier mes informations"></a>               
+</div>   
+
+<div class="button2">
+<a href="../reinitialisationmdp/réuni.php"> <input type="button" value ="Réinitialiser mon mot de passe"></a> </div>
+    </div>
+    	
+    		
+    			
+</section>   
+  
