5fc04fc61731bb3d5c35df0b66ef8c1a9d639a44
Bruno <brunopitaud@gmail.com>
2019-01-23 15:50:22 +0100

    CGU english

--- Views/english/cgu.php
+++ Views/english/cgu.php
@@ -2,9 +2,9 @@
 session_start();
 $_SESSION['language'] ='en';
 
-// if(!isset($_SESSION["connexion"]) or $_SESSION["connexion"] = 0  or empty($_SESSION["connexion"])) {
-//     header("Location: connexion.php");
-// }
+if(!isset($_SESSION["connexion"]) || $_SESSION["connexion"] == 0) {
+    header("Location: connexion.php");
+}
 ?>
 
 <!DOCTYPE html>
@@ -23,109 +23,131 @@ $_SESSION['language'] ='en';
     	<div class="retour">
             <a href="javascript:history.go(-1)">< Retour</a>
         </div>
-        <h1> Conditions générales d'utilisation du site domisep.website</h1>
+        <h1> General condictions of the web site domisep.website</h1>
 
 <div class ="conditions">
 
-<h3> ARTICLE 1 : Objet </h3>
+<h3> ARTICLE 1 : Object </h3>
 <br> </br>
-<p> Les présentes « conditions générales d'utilisation » ont pour objet l'encadrement juridique de l’utilisation du site domisep.website et de ses services. </p>
-<p>Ce contrat est conclu entre :</p>
-<p>Le gérant du site internet, ci-après désigné « l’Éditeur »,</p>
-<p>Toute personne physique ou morale souhaitant accéder au site et à ses services, ci-après appelé « l’Utilisateur ». </p>
-<p>Les conditions générales d'utilisation doivent être acceptées par tout Utilisateur, et son accès au site vaut acceptation de ces conditions.</p>
+<p> 
+The general conditions of use are for the legal framework of the utilisation of the website domisep.website and its services. </p>
+<p>This contract is concluded between : </p>
+<p>The manager of the website, which is mentioned by « The Publisher »,</p>
+<p>
+Any natural or legal person wishing to access the site and its services, is mentionned as "the User".</p>
+<p>The general conditions of use must be accepted by any User, and his access to the site constitutes acceptance of these conditions.</p>
 <br> </br>
-<h3 > ARTICLE 2 : Mentions légales </h3>
+<h3 > ARTICLE 2 : Legal Notice </h3>
 <br> </br>
-Pour les personnes morales :
-<p>Le site domisep.website est édité par la société WEBAC , domicilié au 10 rue de Vanves, 92130, Issy-les-Moulineaux.</p>
-<p>La société est représentée par WEBAC.</p>
-<p>Pour les personnes physiques : </p>
-Le site domisep.website est édité par Alexis BdB, Charlotte B, William C, Emma G, Bruno P.
+For legal persons :
+<p>The website domisep.website is edited by the society WEBAC , resident at 10 rue de Vanves, 92130, Issy-les-Moulineaux.</p>
+<p>The society is represented by WEBAC.</p>
+<p>For individuals : </p>
+The website domisep.website is edited by Alexis BdB, Charlotte B, William C, Emma G, Bruno P.
 <br> </br>
-<h3 > ARTICLE 3 : accès aux services </h3>
+<h3 > ARTICLE 3 : access to services </h3>
 <br> </br>
-L’Utilisateur du site domisep.website a accès aux services suivants :
-<p>•	Ajout, modification et suppression d’un logement ;</p>
-<p>•	Ajout, modification et suppression d’une pièce ;</p>
-<p>•	Ajout, modification et suppression d’un capteur ;</p>
-<p>•	Allumer et éteindre les capteurs ;</p>
-<p>•	Gestion de son espace client.</p>
 
-<p>Tout Utilisateur ayant accès à internet peut accéder gratuitement et depuis n’importe où au site. Les frais supportés par l’Utilisateur pour y accéder (connexion internet, matériel informatique, etc.) ne sont pas à la charge de l’Éditeur.</p>
+The user of the website domisep.website has access to the following services:<p>•	Add, modification and suppression of an house ;</p>
+<p>•	Add, modification and suppression of a piece ;</p>
+<p>•	Add, modification and suppression of a sensor ;</p>
+<p>•	Switch on and switch off the sensors ;</p>
+<p>•	Management of his client area.</p>
 
-<p>Les services suivants ne sont accessibles pour l’Utilisateur que s’il est membre du site (c’est-à-dire qu’il est identifié à l’aide de ses identifiants de connexion) :</p>
-<p>•	Espace client ;</p>
-<p>•	Liste des logements ;</p>
-<p>•	Liste des pièces ; </p>
-<p>•	Liste des capteurs ;</p>
-<p>•	Gestion des capteurs ;</p>
+<p>Any user having access to the internet can access for free from anywhere on the website. The costs incurred by the User to access them (internet connection, computer equipment, etc.) are not the responsibility of the Publisher.</p>
 
-<p>Le site et ses différents services peuvent être interrompus ou suspendus par l’Éditeur, notamment à l’occasion d’une maintenance, sans obligation de préavis ou de justification.</p>
+<p>
+The following services are only accessible for the User if he is a member of the website (he is identified by his login credentials)</p>
+<p>•	Customer Area ;</p>
+<p>•	List of housing ;</p>
+<p>•	Liste of pieces ; </p>
+<p>•	Liste of sensors ;</p>
+<p>•	Sensor management ;</p>
 
-<br> </br>
-<h3> ARTICLE 4 : Responsabilité de l’Utilisateur </h3>
-<br> </br>
-<p>L'Utilisateur est responsable des risques liés à l’utilisation de son identifiant de connexion et de son mot de passe. </p>
-<p>Le mot de passe de l’Utilisateur doit rester secret. En cas de divulgation de mot de passe, l’Éditeur décline toute responsabilité.</p>
-<p>L’Utilisateur assume l’entière responsabilité de l’utilisation qu’il fait des informations et contenus présents sur le site [votre site].</p>
-<p>Tout usage du service par l'Utilisateur ayant directement ou indirectement pour conséquence des dommages doit faire l'objet d'une indemnisation au profit du site.</p>
-
-<p>Le site permet aux membres de publier sur le site :</p>
-<p>•	Logements ;</p>
-<p>•	pièces ;</p>
-<p>•	capteurs .</p>
-
-<p>Le membre s’engage à tenir des propos respectueux des autres et de la loi et accepte que ces publications soient modérées ou refusées par l’Éditeur, sans obligation de justification. </p>
-<p>En publiant sur le site, l’Utilisateur cède à la société éditrice le droit non exclusif et gratuit de représenter, reproduire, adapter, modifier, diffuser et distribuer sa publication, directement ou par un tiers autorisé. </p>
-<p>L’Éditeur s'engage toutefois à citer le membre en cas d’utilisation de sa publication. </p>
-<br> </br>
-<h3> ARTICLE 5 : Responsabilité de l’Éditeur </h3>
-<br> </br>
-<p>Tout dysfonctionnement du serveur ou du réseau ne peut engager la responsabilité de l’Éditeur.</p>
-<p>De même, la responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.</p>
-<p>Le site domisep.website s'engage à mettre en œuvre tous les moyens nécessaires pour garantir la sécurité et la confidentialité des données. Toutefois, il n’apporte pas une garantie de sécurité totale.</p>
-<p>L’Éditeur se réserve la faculté d’une non-garantie de la fiabilité des sources, bien que les informations diffusées sur le site soient réputées fiables.</p>
-<br> </br>
-<h3> ARTICLE 6 : Propriété intellectuelle </h3>
-<br> </br>
-<p>Les contenus du site domisep.website (logos, textes, éléments graphiques, vidéos, etc.) sont protégés par le droit d’auteur, en vertu du Code de la propriété intellectuelle.</p>
-<p>L’Utilisateur devra obtenir l’autorisation de l’éditeur du site avant toute reproduction, copie ou publication de ces différents contenus.</p>
-<p>Ces derniers peuvent être utilisés par les utilisateurs à des fins privées ; tout usage commercial est interdit.</p>
-<p>L’Utilisateur est entièrement responsable de tout contenu qu’il met en ligne et il s’engage à ne pas porter atteinte à un tiers.</p>
-<p>L’Éditeur du site se réserve le droit de modérer ou de supprimer librement et à tout moment les contenus mis en ligne par les utilisateurs, et ce sans justification.</p>
+<p>The website and its various services may be interrupted or suspended by the Publisher, in particular during maintenance, without obligation of notice or justification.</p>
 
 <br> </br>
-<h3> ARTICLE 7 : Données personnelles </h3>
+<h3> ARTICLE 4 : Responsability of the User </h3>
 <br> </br>
-<p>L’Utilisateur doit obligatoirement fournir des informations personnelles pour procéder à son inscription sur le site. 
-</p><p>L’adresse électronique (e-mail) de l’utilisateur pourra notamment être utilisée par le site domisep.website pour la communication d’informations diverses et la gestion du compte.
-</p><p>Domisep.website garantie le respect de la vie privée de l’utilisateur, conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.
-</p><p>Le site est déclaré auprès de la CNIL sous le numéro suivant : 1234567890.
-</p><p>En vertu des articles 39 et 40 de la loi en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit via :
+<p>
+The User is responsible for the risks associated with the use of his login and password. </p>
+<p>
+The password of the User must remain secret. In case of password disclosure, the Publisher declines all responsibility.</p>
+<p>
+The User assumes full responsibility for the use he makes of the information and content on the site domisep.website.</p>
+<p>Any use of the service by the User directly or indirectly resulting in damages must be compensated for the benefit of the site.</p>
 
-    </p><p>•	Son espace personnel sur le site ;
-    </p><p>•	Par mail à no.reply.domisep@gmail.com ;
-    </p><p>•	Par voie postale au 23 rue Notre-Dame des Champs.
+<p>
+The site allows members to publish on the site:</p>
+<p>•	Housing ;</p>
+<p>•	pieces ;</p>
+<p>•	sensors .</p>
+
+<p>
+The member undertakes to make respectful remarks to others and about the law and accepts that these publications are moderate or refused by the Publisher, without obligation of justification. </p>
+<p>
+By posting on the site, the User assigns to the publishing company the non-exclusive and free right to represent, reproduce, adapt, modify and distribute its publication, directly or by an authorized third party. </p>
+<p>The Publisher undertakes, however, to cite the member in case of use of its publication. </p>
+<br> </br>
+<h3> ARTICLE 5 : Publisher responsability </h3>
+<br> </br>
+<p>
+Any malfunction of the server or the network can not engage the responsibility of the Publisher.</p>
+<p>In the same way, the responsibility of the website can not be committed in case of major strenght or the unpredictable and insurmountable fact of a third.</p>
+<p>
+The website domisep.website is committed to implement all necessary means to ensure the security and confidentiality of datas. However, it does not provide a guarantee of total security.</p>
+<p>The Publisher reserves the right of a non-guarantee of the reliability of the sources, although the information diffused on the site is reputed reliable.</p>
+<br> </br>
+<h3> ARTICLE 6 : 
+Intellectual property</h3>
+<br> </br>
+<p>The contents of the website domisep.website (logos, texts, graphic elements, videos, etc.) are protected by copyright, under the Code of Intellectual Property.</p>
+<p>
+The User must obtain the permission of the publisher of the site before any reproduction, copy or publication of these various contents.</p>
+<p>
+These can be used by users for private purposes; any commercial use is prohibited.</p>
+<p>
+The User is entirely responsible for any content he uploads and he undertakes not to infringe on a third party.</p>
+<p>
+The Site Publisher reserves the right to freely moderate or delete content posted by users at any time, without justification.</p>
 
 <br> </br>
-<h3> ARTICLE 8 : Liens hypertextes </h3>
+<h3> ARTICLE 7 : Personal data </h3>
 <br> </br>
-</p><p>Les domaines vers lesquels mènent les liens hypertextes présents sur le site n’engagent pas la responsabilité de l’Éditeur de domisep.website, qui n’a pas de contrôle sur ces liens.
-</p><p>Il est possible pour un tiers de créer un lien vers une page du site [votre site] sans autorisation expresse de l’éditeur.
+<p>
+The User must provide personal information to register on the website.
+</p><p>The e-mail address (e-mail) of the user can be used by the site domisep.website for the communication of various information and management of the account.
+
+</p><p>
+Domisep.website guarantees the respect of the private life of the user, in accordance with the law n ° 78-17 of January 6th, 1978 relating to data processing, files and freedoms.
+</p><p>The site is declared to the CNIL under the following number: 1234567890.
+</p> <p> Pursuant to Articles 39 and 40 of the law dated January 6, 1978, the User has the right to access, rectify, delete and oppose his personal data. The User exercises this right via:
+
+    </p><p>•	His personal space on the site ;
+    </p><p>•	By mail to no.reply.domisep@gmail.com ;
+    </p><p>•	By post at 23 rue Notre-Dame des Champs.
+
 <br> </br>
-<h3> ARTICLE 9 : Évolution des conditions générales d’utilisation </h3>
+<h3> ARTICLE 8 : Hypertext links </h3>
 <br> </br>
-</p><p>Le site domisep.website se réserve le droit de modifier les clauses de ces conditions générales d’utilisation à tout moment et sans justification.
+</p><p>The domains to which the hypertext links present on the site lead do not engage the responsibility of the Editor of domisep.website, which does not have control over these links.
+</p><p>It is possible for a third party to create a link to a page of the website domisep.website without the express permission of the publisher.
+
 <br> </br>
-<h3> ARTICLE 10 : Durée du contrat </h3>
+<h3> ARTICLE 9 : Evolution of the general conditions of use </h3>
 <br> </br>
-</p><p>La durée du présent contrat est indéterminée. Le contrat produit ses effets à l'égard de l'Utilisateur à compter du début de l’utilisation du service.
+</p><p>The website domisep.website reserves the right to modify the clauses of these terms of use at any time and without justification.
 <br> </br>
-<h3> ARTICLE 11 : Droit applicable et juridiction compétente </h3>
+<h3> ARTICLE 10 : Contract length </h3>
 <br> </br>
-</p><p>Le présent contrat dépend de la législation française. 
-</p><p>En cas de litige non résolu à l’amiable entre l’Utilisateur et l’Éditeur, les tribunaux de Paris sont compétents pour régler le contentieux. </p>
+</p><p>
+The lenght of this contract is indefinite. The contract has effect with respect to the User from the beginning of the use of the service.
+<br> </br>
+<h3> ARTICLE 11 : 
+Applicable law and competent jurisdiction </h3>
+<br> </br>
+</p><p>This contract is subject to French law. 
+</p><p>In case of unresolved dispute between the User and the Publisher, the courts of Paris are competent to settle the dispute. </p>
 <br> </br>
 </div>
 </section>
